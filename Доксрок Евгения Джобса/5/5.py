b64465317e0d4078f79284a9a0f17aab25551305
LirokKuku <tumypak@gmail.com>
2023-04-20 17:03:00 +0300

    20/04/2023

--- Доксрок Евгения Джобса/5/5.py
+++ Доксрок Евгения Джобса/5/5.py
@@ -1,5 +1,10 @@
-for i in range(1, 10000):
-    s = bin(i)[2::]
-    if i % 3 == 0:
-        s = s + s[-1:-4:-1]
-    if
+for N in range(1, 10000):
+    R = bin(N)[2::]
+    if N % 3 == 0:
+        R = R + R[-1:-4:-1]
+    else:
+        N = N * 3
+        N = N % 3
+        R = R + bin(N)[2::]
+    if int(R, 2) < 100:
+        print(N)
