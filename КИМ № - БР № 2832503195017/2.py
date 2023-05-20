1ccd731da37c9a9a9a0039658719998c320fa224
LirokKuku <tumypak@gmail.com>
2023-05-21 00:49:28 +0300

    21/05/2023

--- КИМ № - БР № 2832503195017/2.py
+++ КИМ № - БР № 2832503195017/2.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-print('x y z w')
-for x in (0, 1):
-    for y in (0, 1):
-        for z in (0, 1):
-            for w in (0, 1):
-                word = 
-=======
 print('a b c d')
 for a in (0, 1):
     for b in (0, 1):
@@ -14,4 +6,3 @@ for a in (0, 1):
                 f = ((a and b) == (not c)) and (b <= d)
                 if f == 1:
                     print(a,b,c,d, f)
->>>>>>> origin/main
